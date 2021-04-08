120b8114f6f8aa6132d7c0936c4641867c35c05b
WagnerRangelJr <wrangeljr@gmail.com>
2021-04-08 19:59:22 -0300

    React will work with dndprovider and html5backend

--- xadrez-react/src/index.tsx
+++ xadrez-react/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {DndProvider} from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend';
 
 ReactDOM.render(
   <React.StrictMode>
+    <DndProvider backend={HTML5Backend}>
     <App />
+    </DndProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
