3702a4830b17eab087137db78a7a11e03d8c7bfc
WagnerRangelJr <wrangeljr@gmail.com>
2021-04-29 18:33:03 -0300

    Win condition, now acepts checkmate, stalemate, draw

--- xadrez-react/src/Game.js
+++ xadrez-react/src/Game.js
@@ -4,7 +4,10 @@ import {BehaviorSubject} from 'rxjs';
 
 let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
 //let promotion = ' rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
-const chess = new Chess(promotion)
+let checkmate = 'rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3'
+let staleMate = '4k3/4P3/4K3/8/8/8/8/8 b - - 0 78'
+
+const chess = new Chess(checkmate)
 
 
 export const gameSubject = new BehaviorSubject()
@@ -92,9 +95,35 @@ export function move(from, to, promotion){
 }
 
 function updateGame(pendingPromotion){
+    const isGameOver = chess.game_over()
     const newGame = {
         board: chess.board(),
-        pendingPromotion
+        pendingPromotion,
+        isGameOver,
+        result: isGameOver ? getGameResult(): null
     }
     gameSubject.next(newGame)
+}
+
+function getGameResult() {
+    if (chess.in_checkmate()) {
+        const winner = chess.turn() === "w" ? 'BLACK' : 'WHITE'
+        return `CHECKMATE - WINNER - ${winner}`
+    } else if (chess.in_draw()) {
+        let reason = '50 - MOVES - RULE'
+        if (chess.in_stalemate()) {
+            reason = 'STALEMATE'
+        } else if (chess.in_threefold_repetition()) {
+            reason = 'REPETITION'
+        } else if (chess.insufficient_material()) {
+            reason = "INSUFFICIENT MATERIAL"
+        }
+        return `DRAW - ${reason}`
+    } else {
+        return 'UNKNOWN REASON'
+    }
+}
+export function resetGame() {
+    chess.reset()
+    updateGame()
 }
