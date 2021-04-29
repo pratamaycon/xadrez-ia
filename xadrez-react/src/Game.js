be20177993baa7d583c3d992763acd4cb35f65aa
WagnerRangelJr <wrangeljr@gmail.com>
2021-04-29 20:17:58 -0300

    Oh good done a pittyfull  minimax tree

--- xadrez-react/src/Game.js
+++ xadrez-react/src/Game.js
@@ -7,7 +7,7 @@ let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
 let checkmate = 'rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3'
 let staleMate = '4k3/4P3/4K3/8/8/8/8/8 b - - 0 78'
 
-const chess = new Chess(checkmate)
+const chess = new Chess(staleMate)
 
 
 export const gameSubject = new BehaviorSubject()
@@ -29,18 +29,40 @@ export function initGame(){
 }*/
 
 export function handleMove(from, to){
+
+    if (!chess.in_checkmate() && !chess.in_stalemate() && !chess.in_stalemate() && !chess.in_threefold_repetition() && !chess.insufficient_material()   ){
     const promotions = chess.moves({ verbose: true }).filter(m => m.promotion)
     // eslint-disable-next-line
     //const moves = chess.moves({verbose: true})
     console.table(promotions)
-    /*if (moves[1].color === "b"){
-        console.log("**************")
-        const moveia = moves.[Math.floor(Math.random() * moves.length)]
-        console.log(moveia)
 
-        move(moveia.from , moveia.to)
+    //IA POSSIBLE-MOVES
+    const possibleMoves = chess.moves({verbose: true})
+    console.log(possibleMoves)
+
+    
+    if (possibleMoves[1].color === "b"){
+        //moveEvaluetionWarnig()
+        const possibleMoves = chess.moves({verbose: true})
+        const possibleCapture = chess.moves({verbose: true}).filter(m => m.captured)
+        console.log("************** Possible Moves **************")
+        console.table(possibleMoves)
+        console.log("************** Possible Caputres **************")
+        console.log(possibleCapture)
+        console.table(possibleCapture)
+        if(possibleCapture.length !== 0){
+            const moveIaCapture = possibleCapture.[Math.floor(Math.random() * possibleCapture.length)]
+            console.log(possibleCapture)
+            move(moveIaCapture.from , moveIaCapture.to)
+            console.log(moveIaCapture)
+        } else {
+            const moveia = possibleMoves.[Math.floor(Math.random() * possibleMoves.length)]
+            console.log(moveia)
+
+            move(moveia.from , moveia.to)
+        }
     }
-    else{
+    /*else{
         console.log(moves)
         move(from,to)
     } */
@@ -55,7 +77,7 @@ export function handleMove(from, to){
     if (!pendingPromotion){
         move(from, to)
     }
-    
+}
 }
 
 export function iaHandleMove(from, to){
@@ -125,8 +147,8 @@ function getGameResult() {
     }
 }
 
-function moveEvaluetion(){
-    	let nume = true;
+function moveEvaluetionWarnig(){
+    	alert("Clica em uma peca preta para mover a IA")
 }
 
 export function resetGame() {
